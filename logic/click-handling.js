f392136871063fb7e86d75decb252a28be18df8d
Diwas Mainali <diwas.sok@gmail.com>
2024-02-06 22:06:51 +1100

    AddedHighlights

--- logic/click-handling.js
+++ logic/click-handling.js
@@ -1,20 +1,50 @@
 const allSquares = document.getElementsByClassName("square");
 var isClicked = false;
 
-const clickedElements = []
+const clickedElements = [];
+const pawnElements = [];
 for(const i of allSquares) {
     i.addEventListener("click", function() {
-        const idOfElement = i.getAttribute("id");
-        const innerHTMLOfEl = document.getElementById(idOfElement).innerHTML;
         if(clickedElements.length > 0) {
             clickedElements[0].removeAttribute("style");
         }
-        console.log(clickedElements.length);
+        if(pawnElements.length > 0) {
+            pawnElements[0].removeAttribute("style");
+            pawnElements[1].removeAttribute("style");
+        }
+        const idOfElement = i.getAttribute("id");
+        const innerHTMLOfEl = document.getElementById(idOfElement).innerHTML;
+        const images = this.getElementsByTagName("img");
         if(
             (innerHTMLOfEl.includes("black") ||
             innerHTMLOfEl.includes("white")) && i !== clickedElements.pop()) {
                 document.getElementById(idOfElement).style.backgroundColor = '#BBCC43';
                 clickedElements.push(i);
         }
+        for(const img of images) {
+            if(img.src.includes("pawn")) {
+                console.log(idOfElement, "Pawn!");
+                pawnMoves(idOfElement, pawnElements);            
+            }
+        }
+        console.log(pawnElements.length, "pawn length");
+
     });
 }
+
+function pawnMoves(elementId, pawnElements) {
+    const lastElement = pawnElements.pop();
+    const secondLastElement = pawnElements.pop();
+    if (elementId.includes("7") && document.getElementById(elementId[0] + "5") !== lastElement && document.getElementById(elementId[0] + "6") !== secondLastElement) {
+        const firstMove = document.getElementById(elementId[0] + "6");
+        const secondMove = document.getElementById(elementId[0] + "5");
+        
+        firstMove.style.backgroundColor = "yellow";
+        firstMove.style.border = "0.1px solid black";
+        firstMove.style.boxSizing = "border-box";
+        secondMove.style.backgroundColor = "yellow";
+        secondMove.style.border = "0.1px solid black";
+        secondMove.style.boxSizing = "border-box";
+        pawnElements.push(firstMove, secondMove);
+    }
+};
